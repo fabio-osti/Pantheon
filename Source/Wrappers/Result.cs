23b4ab3623262a1c7204555a0e378ff67782c745
Fabio Luiz Alvares Osti <fabio.osti@hotmail.com>
2023-01-20 18:21:07 -0300

    Added empty result.

--- Source/Wrappers/Result.cs
+++ Source/Wrappers/Result.cs
@@ -2,7 +2,8 @@ namespace OmnisExtensionum.Wrappers;
 
 /// <summary>
 /// 	Represents a wrapper that may or may not
-///		contain a <typeparamref name="TValue"/>. 	
+///		contain a <typeparamref name="TValue"/>
+///		result. 	
 /// </summary>
 /// 
 /// <typeparam name="TValue">
@@ -64,4 +65,52 @@ public struct Result<TValue>
 	/// 	is successful (<see cref="Value"/> is not null).
 	/// </summary>
 	public bool IsSuccessful => _value is not null;
+}
+
+/// <summary>
+/// 	Represents if a result was completed successfully. 	
+/// </summary>
+public struct Result
+{
+	/// <summary>
+	/// 	Initializes a failed <see cref="Result{TValue}"/>
+	/// 	with <paramref name="exception"/>.
+	/// </summary>
+	/// 
+	/// <param name="exception">
+	/// 	Cause of failure.
+	/// </param>
+	public Result(Exception exception)
+	{
+		_exception = exception;
+	}
+
+	/// <summary>
+	/// 	Initializes a successful <see cref="Result{TValue}"/>.
+	/// </summary>
+	public Result()
+	{
+	}
+
+	private Exception? _exception;
+	/// <summary>
+	/// 	Gets the cause of failure or throws if 
+	/// 	<see cref="IsSuccessful"/> is true.
+	/// </summary>
+	/// 
+	///	<exception cref="InvalidOperationException">
+	/// 	If <see cref="IsSuccessful"/> is true.
+	/// </exception>
+	public Exception Exception =>
+		_exception
+			??
+		throw new InvalidOperationException(
+			$"The action was successful."
+		);
+
+	/// <summary>
+	/// 	Gets wether this <see cref="Result{TValue}"/>
+	/// 	is successful (<see cref="Exception"/> is null).
+	/// </summary>
+	public bool IsSuccessful => Exception is null;
 }
